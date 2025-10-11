af7bcaa46a0c884f9704b9de2a015642579d4dee
Prosperityamadi <133218616+Prosperityamadi@users.noreply.github.com>
2025-10-11 21:59:31 +0000

    Add SharedPreferences init

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:spar/routes/routes.dart';
 import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // Pre-initialize SharedPreferences to avoid lookup errors
+  await SharedPreferences.getInstance();
 
   // Lock orientation to portrait only
   await SystemChrome.setPreferredOrientations([
