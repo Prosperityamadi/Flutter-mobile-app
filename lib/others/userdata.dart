648a305f8d977626f8e9d2c002a7e01b8c27a0dd
Prosperityamadi <133218616+Prosperityamadi@users.noreply.github.com>
2025-10-17 18:42:12 +0000

    feat: Add User class with uid and phoneNumber

--- lib/others/userdata.dart
+++ lib/others/userdata.dart
@@ -2,3 +2,9 @@ class UserData {
   static String? phoneNumber;
   static String? userName;
 }
+
+class User {
+  final String? uid;
+  final String? phoneNumber;
+  User({required this.uid, this.phoneNumber});
+}
